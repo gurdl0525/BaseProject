6d90f8b0975e9a87ae3f4c349958c901942e81ca
squall7011 <gurdl7011@dsm.hs.kr>
2022-07-19 23:44:27 +0900

    POST:::create JwtConfigurer

---
+++ src/main/java/com/example/refreshtoken/global/security/JwtConfigurer.java
@@ -0,0 +1,20 @@
+package com.example.refreshtoken.global.security;
+
+import com.example.refreshtoken.global.error.ExceptionHandlerFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+    @Override
+    public void configure(HttpSecurity builder){
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
+    }
+}
