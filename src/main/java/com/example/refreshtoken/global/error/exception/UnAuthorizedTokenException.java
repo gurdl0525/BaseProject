ad920823314a6a8db858e95f3cc3a6713560104a
squall7011 <gurdl7011@dsm.hs.kr>
2022-07-19 23:45:05 +0900

    POST:::create UnAuthorizedTokenException

---
+++ src/main/java/com/example/refreshtoken/global/error/exception/UnAuthorizedTokenException.java
@@ -0,0 +1,16 @@
+package com.example.refreshtoken.global.error.exception;
+
+import com.example.refreshtoken.global.error.ErrorCode;
+
+public class UnAuthorizedTokenException extends BaseException {
+    private UnAuthorizedTokenException(){
+        super(ErrorCode.UN_AUTHORIZED_TOKEN_EXCEPTION);
+    }
+
+    private static class SingletonHelper{
+        private static final UnAuthorizedTokenException INSTANCE = new UnAuthorizedTokenException();
+    }
+    public static UnAuthorizedTokenException getInstance(){
+        return UnAuthorizedTokenException.SingletonHelper.INSTANCE;
+    }
+}
