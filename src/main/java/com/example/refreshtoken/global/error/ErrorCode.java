5d9ef3d5416df5a54d8c5dcf9dac78d40578af21
squall7011 <gurdl7011@dsm.hs.kr>
2022-07-27 20:45:18 +0900

    UPDATE:::에러코드 메세지 수정

--- src/main/java/com/example/refreshtoken/global/error/ErrorCode.java
+++ src/main/java/com/example/refreshtoken/global/error/ErrorCode.java
@@ -6,12 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    DUPLICATE_MEMBER(400, "DUPLICATE MEMBER : 중복된 회원입니다."),
-    UN_AUTHORIZED_TOKEN_EXCEPTION(401, "UN_AUTHORIZED_TOKEN_EXCEPTION : 인증되지 않은 토큰입니다."),
+    DUPLICATE_MEMBER(400, "Duplicate Member."),
+    UN_AUTHORIZED_TOKEN_EXCEPTION(401, "Un Authorized Token."),
     INVALID_TOKEN(401, "Invalid Token."),
-    PASSWORD_INCONSISTENCY(401, "PASSWORD_NOT_MATCHED : 비밀번호가 틀렸습니다."),
-    NOT_FOUND_USER(404, "NOT_FOUND_USER : 사용자를 찾을 수 없습니다."),
-    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR : 서버 내부 에러입니다.");
+    PASSWORD_INCONSISTENCY(401, "Password Not Matched."),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error.");
 
     private final Integer status;
     private final String message;
