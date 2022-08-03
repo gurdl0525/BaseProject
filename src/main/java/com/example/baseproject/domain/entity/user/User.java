c4c577d5b6398d9491d22733c98ac3d88b658780
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-03 20:54:14 +0900

    UPDATE:::코드 정렬

--- src/main/java/com/example/baseproject/domain/entity/user/User.java
+++ src/main/java/com/example/baseproject/domain/entity/user/User.java
@@ -14,11 +14,14 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "account_id")
     private String accountId;
+
     @Column(name = "password")
     private String password;
 }
