4ef6800e19015c0569b570e8e5297511decd0d74
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-03 20:55:37 +0900

    UPDATE:::NOT_FOUND_USER

--- src/main/java/com/example/baseproject/global/error/ErrorCode.java
+++ src/main/java/com/example/baseproject/global/error/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     UN_AUTHORIZED_TOKEN_EXCEPTION(401, "Un Authorized Token."),
     INVALID_TOKEN(401, "Invalid Token."),
     PASSWORD_INCONSISTENCY(401, "Password Not Matched."),
+    NOT_FOUND_USER(404, "Not Found User"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error.");
 
     private final Integer status;
