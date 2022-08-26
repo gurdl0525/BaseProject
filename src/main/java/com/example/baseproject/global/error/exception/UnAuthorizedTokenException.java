c833286fd47a74a1e53687e494d012633d4accd1
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-26 18:57:19 +0900

    ♠ UPDATE ::: 코드정렬

--- src/main/java/com/example/baseproject/global/error/exception/UnAuthorizedTokenException.java
+++ src/main/java/com/example/baseproject/global/error/exception/UnAuthorizedTokenException.java
@@ -3,6 +3,7 @@ package com.example.baseproject.global.error.exception;
 import com.example.baseproject.global.error.ErrorCode;
 
 public class UnAuthorizedTokenException extends BaseException {
+
     private UnAuthorizedTokenException(){
         super(ErrorCode.UN_AUTHORIZED_TOKEN_EXCEPTION);
     }
@@ -10,6 +11,7 @@ public class UnAuthorizedTokenException extends BaseException {
     private static class SingletonHelper{
         private static final UnAuthorizedTokenException INSTANCE = new UnAuthorizedTokenException();
     }
+
     public static UnAuthorizedTokenException getInstance(){
         return SingletonHelper.INSTANCE;
     }
