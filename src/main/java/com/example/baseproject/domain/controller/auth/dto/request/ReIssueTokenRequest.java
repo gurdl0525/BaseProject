4da3af995b8757d829beb58f9d003a261dc3a2af
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-26 18:59:23 +0900

    ♠ UPDATE ::: 불필요 어노테이션 제거 및 final추가

--- src/main/java/com/example/baseproject/domain/controller/auth/dto/request/ReIssueTokenRequest.java
+++ src/main/java/com/example/baseproject/domain/controller/auth/dto/request/ReIssueTokenRequest.java
@@ -1,15 +1,11 @@
 package com.example.baseproject.domain.controller.auth.dto.request;
 
-import com.sun.istack.NotNull;
 import lombok.*;
 
 @Builder
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReIssueTokenRequest {
 
-    @NotNull
-    private String refreshToken;
+    private final String refreshToken;
 
 }
