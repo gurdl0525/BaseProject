f14bfdd710a0698f054f3047a035f5c98c0d0fbc
squall7011 <gurdl7011@dsm.hs.kr>
2022-08-26 18:56:49 +0900

    ♠ UPDATE ::: 코드정렬

--- src/main/java/com/example/baseproject/global/error/ExceptionHandlerFilter.java
+++ src/main/java/com/example/baseproject/global/error/ExceptionHandlerFilter.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
     public void setErrorResponse(ErrorCode errorCode, HttpServletResponse response, Throwable e){
         response.setStatus(errorCode.getStatus());
         response.setContentType("application/json");
@@ -27,6 +28,7 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
             ex.printStackTrace();
         }
     }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
